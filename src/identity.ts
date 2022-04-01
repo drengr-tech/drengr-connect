88482a3f2e0ff67661a4ffe1caf0b4d4ac6f679b
Pierre Huchot <pierre.huchot@gmail.com>
2022-04-01 17:34:43 +0200

    Integrating wallet connect

--- src/identity.ts
+++ src/identity.ts
@@ -1,33 +1,55 @@
-import {ethers, Signer} from "ethers"
+import { ethers, Signer } from "ethers"
 import web3modal, { Modal } from "web3modal"
 import { SiweMessage } from "siwe";
 import axios from "axios";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 
 export interface PendingVerification {
     address: string;
     nonce: string;
-} 
+}
 
 export interface VerificationResponse {
     address: string,
-    nonce : string,
+    nonce: string,
     verified: boolean
 }
 
 export class Wallet {
 
     signer!: Signer;
-    modal : web3modal;
+    modal: web3modal;
     address!: string;
     chainId!: number;
     connected = false;
 
-    constructor(){
-        this.modal = new web3modal({});
+    constructor() {
+        this.modal = new web3modal({
+            providerOptions: {
+               
+                walletconnect: {
+                    package: WalletConnectProvider, // required
+                    options: {
+                        infuraId: "79ba2ce2ebe64d86b8ecce3d234b89de", // required,
+                        qrcodeModalOptions: {
+                            mobileLinks: [
+                                "metamask",
+                              "rainbow",
+                              "argent",
+                              "trust",
+                              "imtoken",
+                              "pillar",
+                            ],
+                          },
+                    }
+                }
+
+            }
+        });
     }
 
-    async connect(){
+    async connect() {
         const web3provider = await this.modal.connect();
 
         const provider = new ethers.providers.Web3Provider(web3provider);
@@ -39,14 +61,14 @@ export class Wallet {
         this.connected = true;
     }
 
-    async addInfos(){
+    async addInfos() {
         this.address = await this.signer.getAddress();
         this.chainId = await this.signer.getChainId();
     }
 
-    async getVerification(nonce : string = ""){
+    async getVerification(nonce: string = "") {
 
-        if(nonce.trim() === ""){
+        if (nonce.trim() === "") {
             nonce = await this.requestNonce();
         }
 
@@ -56,20 +78,20 @@ export class Wallet {
 
         const signature = await this.signMessage(message);
 
-        return {...infos, signature};
+        return { ...infos, signature };
     }
 
-    async requestNonce(){
+    async requestNonce() {
 
         const response = await axios.get(`https://api.drengr.io/verification/nonce/${this.address}`);
 
-        const pendingVerification : PendingVerification = response.data;
+        const pendingVerification: PendingVerification = response.data;
 
         return pendingVerification.nonce;
 
     }
 
-    createVerificationInfos(nonce: string){
+    createVerificationInfos(nonce: string) {
 
         const infos = {
             domain: window.location.host,
@@ -84,11 +106,11 @@ export class Wallet {
         return infos;
     }
 
-    async createVerificationMessage(infos : any){
+    async createVerificationMessage(infos: any) {
         return new SiweMessage(infos);
     }
 
-    async signMessage(message: SiweMessage){
+    async signMessage(message: SiweMessage) {
         return await this.signer.signMessage(message.prepareMessage());
     }
 
@@ -96,23 +118,23 @@ export class Wallet {
 
 const wallet = new Wallet();
 
-export {wallet}
+export { wallet }
 
-export const connect = async () : Promise<string> => {
-    
+export const connect = async (): Promise<string> => {
+
     await wallet.connect();
 
     return wallet.address;
 
 }
 
-export const getVerificationMessage = async (nonce : string = "") : Promise<any> => {
+export const getVerificationMessage = async (nonce: string = ""): Promise<any> => {
 
-    if(nonce.trim() === ""){
+    if (nonce.trim() === "") {
 
     }
 
-    if(!wallet.address){
+    if (!wallet.address) {
         await wallet.connect();
     }
 
