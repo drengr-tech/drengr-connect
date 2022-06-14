a3995b3089a1a5a031e8af09bbf4fc2cf30dce95
Pierre Huchot <pierre.huchot@gmail.com>
2022-06-14 20:11:35 +0200

    Branch 0.1.3-1

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,4 @@
 export const CONTRACT_INTERFACE_V1 = ["function pay(address from, address to) public payable returns(bool)"];
 
 export const CONTRACT_ROPSTEN_V1 = "0xf05eC9616891E480a764439bFd4E7A36a43C37e4";
+export const CONTRACT_MAINNET_V1 = "0x71Fb0C4fBC29C57c63Dced7C9b0eA01734B02996";
--- src/payment.ts
+++ src/payment.ts
@@ -1,6 +1,6 @@
 import { Contract, Signer } from "ethers";
 import { parseEther } from "ethers/lib/utils";
-import { CONTRACT_INTERFACE_V1, CONTRACT_ROPSTEN_V1 } from "./const";
+import { CONTRACT_INTERFACE_V1, CONTRACT_MAINNET_V1, CONTRACT_ROPSTEN_V1 } from "./const";
 import { wallet } from "./identity";
 
 export interface UncompletedPayment {
@@ -17,22 +17,30 @@ export class Payments{
     async pay(receiverAddress: string, amount : number) : Promise<UncompletedPayment>{
 
         const chainId = await this.signer.getChainId();
-    
-        if(chainId === 1){
-            throw "Payments are not available on mainnet yet. Please switch to Ropsten network.";
-        }
-    
-        if(chainId !== 3){
-            throw "Payments testnet is on Ropsten. Please switch to Ropsten network."
-        }
 
-        const paymentContract = new Contract(CONTRACT_ROPSTEN_V1, CONTRACT_INTERFACE_V1, this.signer);
+        let paymentContract: Contract;
+        let paymentContractAddress: string;
+
+        switch(chainId){
+
+            case 1:
+                paymentContractAddress = CONTRACT_MAINNET_V1;
+                break;
+            case 3:
+                paymentContractAddress = CONTRACT_ROPSTEN_V1;
+                break;
+            default:
+                throw "Chain not supported. Use mainnet or Ropsten"
+
+        }
+        
+        paymentContract = new Contract(paymentContractAddress, CONTRACT_INTERFACE_V1, this.signer);
 
         const from = await this.signer.getAddress()
 
         let tx = await paymentContract.pay(from, receiverAddress, {value: parseEther(amount.toString())});
 
-        return {txHash: tx.hash, recipient: receiverAddress, payer: from, isTestnet: true}
+        return {txHash: tx.hash, recipient: receiverAddress, payer: from, isTestnet: chainId === 1 ? false : true}
     
     }
 
